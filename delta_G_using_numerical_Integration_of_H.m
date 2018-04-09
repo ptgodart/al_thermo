9ab31e9b5fec5a4c206ea2b714a8949520469260
JasonFischman <jfischma@mit.edu>
2018-04-09 14:02:29 -0400

    fixed indexing errors, plot results

--- delta_G_using_numerical_Integration_of_H.m
+++ delta_G_using_numerical_Integration_of_H.m
@@ -8,7 +8,8 @@ H_alooh_different_temperatures;
 %%
 % Al2O3: 
 G_al2o3 = zeros(length(H_al2o3),2);
-G_al2o3(1) = -1582; %STP value of G_Al2O3 [kj/mol]
+G_al2o3(1,1) = 300;
+G_al2o3(1,2) = -1582; %STP value of G_Al2O3 [kj/mol]
 
 %calculate gibbs for Al2O3 at different temps: 
 for x  = 2:length(H_al2o3)
@@ -17,15 +18,16 @@ for x  = 2:length(H_al2o3)
     H_a = H_al2o3(x-1,2);
     H_b = H_al2o3(x,2);
     G = T_b*(((T_b- T_a)/6) * ((H_a/(T_a^2)) + 4*(((H_a+ H_b)/2) / (((T_a + T_b)/2)^2)) + (H_b/(T_b^2)))); %Simpson's rule
-    G_al2o3(x-1,1) = T_b;
-    G_al2o3(x-1,2) = (G_al2o3(x-1)*(T_b/T_a) + G); 
+    G_al2o3(x,1) = T_b;
+    G_al2o3(x,2) = (G_al2o3(x-1,2)*(T_b/T_a) - G); 
 end
 
 %%
 % Al(OH)3: 
 
 G_aloh3 = zeros(length(H_al2o3),2);
-G_aloh3(1) = -1305; %%STP value of G_Al(OH)3 [kj/mol] 
+G_aloh3(1,1) = 300;
+G_aloh3(1,2) = -1305; %%STP value of G_Al(OH)3 [kj/mol] 
 
 %calculate gibbs for Al(OH)3 at different temps:
 for x  = 2:length(H_al2o3)
@@ -34,15 +36,16 @@ for x  = 2:length(H_al2o3)
     H_a = H_aloh3(x-1,2);
     H_b = H_aloh3(x,2);
     G = T_b*(((T_b - T_a)/6) * ((H_a/(T_a^2)) + 4*(((H_a+ H_b)/2) / (((T_a + T_b)/2)^2)) + (H_b/(T_b^2)))); %Simpson's rule
-    G_aloh3(x-1,1) = T_b;
-    G_aloh3(x-1,2) = (G_aloh3(x-1)*(T_b/T_a) + G);
+    G_aloh3(x,1) = T_b;
+    G_aloh3(x,2) = (G_aloh3(x-1,2)*(T_b/T_a) - G);
 end 
 
 %%
 % AlOOH: 
 
 G_alooh = zeros(length(H_alooh),2);
-G_alooh(1) = -918; %%STP value of G_AlOOH [kj/mol] 
+G_alooh(1,1) = 300;
+G_alooh(1,2) = -918; %%STP value of G_AlOOH [kj/mol] 
 
 %calculate gibbs for AlOOH at different temps:
 for x  = 2:length(H_alooh)
@@ -51,9 +54,21 @@ for x  = 2:length(H_alooh)
     H_a = H_alooh(x-1,2);
     H_b = H_alooh(x,2);
     G = T_b*(((T_b- T_a)/6) * ((H_a/(T_a^2)) + 4*(((H_a+ H_b)/2) / (((T_a + T_b)/2)^2)) + (H_b/(T_b^2)))); %Simpson's rule
-    G_alooh(x-1,1) = T_b;
-    G_alooh(x-1,2) = (G_alooh(x-1)*(T_b/T_a) + G); 
+    G_alooh(x,1) = T_b;
+    G_alooh(x,2) = (G_alooh(x-1,2)*(T_b/T_a) - G); 
 end 
 
-G_alooh2 = [300	350	400	450	500	550	600;
+G_alooh_confirmation = [300	350	400	450	500	550	600;
     -917.916	-904.72	-891.595	-878.351	-865.153	-851.917	-838.74]';
+
+figure(1)
+plot(G_al2o3(:,1),G_al2o3(:,2))
+%title('G al2o3')
+%figure(2)
+hold on 
+plot(G_alooh(:,1),G_alooh(:,2))
+%title('G alooh')
+%figure(3)
+hold on
+plot(G_aloh3(:,1),G_aloh3(:,2))
+%title('G aloh3')
