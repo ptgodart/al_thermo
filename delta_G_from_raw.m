311a771f59002cca6b1b56e9419cb4f81cf64ce7
Peter Godart <ptgodart@gmail.com>
2018-04-09 14:03:35 -0400

    plotting corrected values for alooh

--- delta_G_from_raw.m
+++ delta_G_from_raw.m
@@ -39,7 +39,7 @@ H298_h2o = 1000 * (h2o_raw_data(1, 6) - h2o_raw_data(1, 3));
 T_h2o = h2o_raw_data(:, 1);
 G_h2o = (h2o_raw_data(:, 5) * -1 .* T_h2o) + H298_h2o;
 
-range = 60;
+range = 30;
 
 %% Al2O3 Reaction
 % 2Al + 3H2O ==> Al2O3 + 3H2
@@ -69,11 +69,32 @@ for t = 1:range
     G_aloh3_rxn(t) = 3*G_h2(t) + 2*G_aloh3(t) - 2*G_al(t) - 6*G_h2o(t);
 end
 
+%% AlO(OH) from Hemingway paper
+% 2Al + 4H2O ==> 2AlO(OH) + 3H2
+H_298 = -996.38E3;
+G_alooh_paper_eq = [37.191 37.880 39.485 41.656 44.180 46.922 49.796]; % J/mol-K
+% Values right from paper
+G_alooh_paper_raw = 1E3*[-917.916 -904.720 -891.595 -878.351 -865.153 -851.917 -838.740];
+T_alooh_paper = [300 350 400 450 500 550 600];
+% Converted values
+G_alooh_paper = -1*T_alooh_paper.*G_alooh_paper_eq + H_298;
+T_alooh_paper_rxn = zeros(7,1);
+G_alooh_paper_rxn_1 = zeros(7,1);
+G_alooh_paper_rxn_2 = zeros(7,1);
+for t = 1:7
+    temp = T_alooh_paper(t);
+    T_alooh_paper_rxn(t) = T_alooh_paper(t);
+    G_alooh_paper_rxn_1(t) = 3*G_h2(t) + 2*G_alooh_paper(t) - 2*G_al(t) - 4*G_h2o(t);
+    G_alooh_paper_rxn_2(t) = 3*G_h2(t) + 2*G_alooh_paper_raw(t) - 2*G_al(t) - 4*G_h2o(t);
+end
+
 %% Plotting
 figure(1);
 clf;
 hold on;
-plot(T_aloh3_rxn, G_aloh3_rxn);
-plot(T_alooh_rxn, G_alooh_rxn);
-plot(T_al2o3_rxn, G_al2o3_rxn);
-legend('Hydroxide', 'Oxyhydroxide', 'Oxide');
+plot(T_aloh3_rxn - 274.15, G_aloh3_rxn);
+%plot(T_alooh_rxn - 274.15, G_alooh_rxn);
+plot(T_al2o3_rxn - 274.15, G_al2o3_rxn);
+plot(T_alooh_paper_rxn - 274.15, G_alooh_paper_rxn_1);
+plot(T_alooh_paper_rxn - 274.15, G_alooh_paper_rxn_2);
+legend('Hydroxide', 'Oxide', 'Oxyhydroxide - paper (free energy)', 'Oxyhydroxide - paper (converted)');
