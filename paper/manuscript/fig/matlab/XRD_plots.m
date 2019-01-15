086c66e9dd235d5ee79c300c69dd73e2c0519dd7
JasonFischman <jfischma@mit.edu>
2019-01-15 16:56:15 -0500

    addition of high temp high pressure XRD and attempts to smooth it and find peaks
    addition of high temp high pressure XRD and attempts to smooth it and find peaks

--- paper/manuscript/fig/matlab/XRD_plots.m
+++ paper/manuscript/fig/matlab/XRD_plots.m
@@ -1,7 +1,7 @@
 XRD_100c_1atm
 XRD_4c_1atm
 XRD_4c_68atm
-
+XRD_230c_68atm
 
 figure(1)
 bar(XRD_highTlowP(:,1),XRD_highTlowP(:,2),.1);
@@ -15,5 +15,15 @@ ylabel('Intensity (Counts)')
 
 figure(3)
 bar(XRD_lowThighP(:,1),XRD_lowThighP(:,2),.1);
+xlabel('2 Theta (Degrees)')
+ylabel('Intensity (Counts)')
+
+figure(4)
+bar(XRD_highThighP_corrected(:,1),XRD_highThighP_corrected(:,2),.1);
+xlabel('2 Theta (Degrees)')
+ylabel('Intensity (Counts)')
+
+figure(5)
+bar(XRD_highThighP(:,1),XRD_highThighP(:,2),.1);
 xlabel('2 Theta (Degrees)')
 ylabel('Intensity (Counts)')
