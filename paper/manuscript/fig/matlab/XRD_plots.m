a544728ba0dca143b2e531f3da72b10a314c88fc
JasonFischman <jfischma@mit.edu>
2019-01-16 14:42:06 -0500

    finished_XRD_plots
    finished_XRD_plots

--- paper/manuscript/fig/matlab/XRD_plots.m
+++ paper/manuscript/fig/matlab/XRD_plots.m
@@ -6,24 +6,33 @@ XRD_230c_68atm
 figure(1)
 bar(XRD_highTlowP(:,1),XRD_highTlowP(:,2),.1);
 xlabel('2 Theta (Degrees)')
+xlim([15 90])
 ylabel('Intensity (Counts)')
+title('E2: 100 C, 0.1 MPa')
 
 figure(2)
 bar(XRD_lowTlowP(:,1),XRD_lowTlowP(:,2),.1);
 xlabel('2 Theta (Degrees)')
+xlim([15 90]);
 ylabel('Intensity (Counts)')
+title('E1: 4 C, 0.1 MPa')
 
 figure(3)
 bar(XRD_lowThighP(:,1),XRD_lowThighP(:,2),.1);
 xlabel('2 Theta (Degrees)')
+xlim([15 90]);
 ylabel('Intensity (Counts)')
+title('E3: 4 C, 6.9 MPa')
 
 figure(4)
-bar(XRD_highThighP_corrected(:,1),XRD_highThighP_corrected(:,2),.1);
+bar(XRD_highThighP(:,1),XRD_highThighP(:,2),.1);
 xlabel('2 Theta (Degrees)')
+xlim([15 90]);
 ylabel('Intensity (Counts)')
 
 figure(5)
-bar(XRD_highThighP(:,1),XRD_highThighP(:,2),.1);
+bar(XRD_highThighP_manual(:,1),XRD_highThighP_manual(:,2),.1);
 xlabel('2 Theta (Degrees)')
+xlim([15 90]);
 ylabel('Intensity (Counts)')
+title('E4: 230 C, 6.9 MPa')
